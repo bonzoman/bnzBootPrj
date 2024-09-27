e3c63037e70ff03439f2370f582122bec6a1ef63
bonzoman <bonzoman@gmail.com>
2024-09-27 22:31:32 +0900

    aaaaaaaaaaaaaaa

--- src/main/java/com/bnz/Application.java
+++ src/main/java/com/bnz/Application.java
@@ -7,16 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        /** case 1 */
+        /** case 1111 */
 //        SpringApplication.run(Application.class, args);
 
-        /** case 2 */
+        /** case 2222 */
       SpringApplication app = new SpringApplication(Application.class);
       app.setWebApplicationType(WebApplicationType.SERVLET);//default임
       app.addListeners(new SampleListener());////SpringContext 생성 전 발생하는 이벤트 리스너
       app.run(args);
 
-        /** case 3 커스터마이징 가능하게 */
+        /** case 3333 커스터마이징 가능하게 */
 //        SpringApplicationBuilder builder = new SpringApplicationBuilder();
 //        builder.sources(Application.class).build();
 //        SpringApplication app = builder.application();
